a9d64585c5b445ed79b838f459d846955f91cc42
Jaluson <2113593019@qq.com>
2026-09-16 11:07:19 +0800

    ci: allow slower Windows shell integration tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({ test: { setupFiles: ['./tests/setup.ts'] } });
+export default defineConfig({ test: { setupFiles: ['./tests/setup.ts'], testTimeout: 15_000 } });
