bfa4acab55cc578af01aed17797bfaeae7c8da3b
Jaluson <2113593019@qq.com>
2026-09-16 11:51:38 +0800

    ci: allow Windows shell tests more startup time

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({ test: { setupFiles: ['./tests/setup.ts'], testTimeout: 15_000 } });
+export default defineConfig({ test: { setupFiles: ['./tests/setup.ts'], testTimeout: 30_000 } });
